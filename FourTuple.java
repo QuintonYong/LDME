dedfd0350e02f677a3ff8dffb9e82bb3f39e7b86
QuintonYong <quintillion.y@gmail.com>
2020-09-21 21:18:54 -0700

    Add files via upload

---
+++ FourTuple.java
@@ -0,0 +1,20 @@
+public class FourTuple implements Comparable<FourTuple> {
+    int A, B, u, v;
+
+    public FourTuple(int A, int B, int u, int v) {
+        this.A = A;
+        this.B = B;
+        this.u = u;
+        this.v = v;
+    }
+
+    public int compareTo(FourTuple other) {
+        if (this.A < other.A || (this.A == other.A && this.B < other.B)) {
+            return -1;
+        } else if (this.A > other.A || (this.A == other.A && this.B > other.B)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
