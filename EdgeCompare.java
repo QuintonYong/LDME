631da600c45a7c403cd65102f2098bc4b7ac4547
QuintonYong <quintillion.y@gmail.com>
2020-09-22 13:01:23 -0700

    Add files via upload

---
+++ EdgeCompare.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+import org.javatuples.Pair;
+
+public class EdgeCompare implements Comparator<Pair<Integer, Integer>>
+{   
+    int[] supernode_sizes;
+
+    public EdgeCompare(int[] supernode_sizes) {
+        this.supernode_sizes = supernode_sizes;
+    }
+
+    public int compare(Pair<Integer, Integer> P1, Pair<Integer, Integer> P2) {
+        int P1_val = supernode_sizes[P1.getValue0()] * supernode_sizes[P1.getValue1()];
+        int P2_val = supernode_sizes[P2.getValue0()] * supernode_sizes[P2.getValue1()];
+
+        if (P1_val < P2_val) { return -1; }
+        else if (P1_val > P2_val) { return 1; }
+        else { return 0; }
+    }
+}
---
+++ OnePermHashSig.java
@@ -0,0 +1,47 @@
+public class OnePermHashSig {
+    int sigSize;
+    int[] sig;
+
+    public OnePermHashSig(int sigSize) {
+        this.sigSize = sigSize;
+        sig = new int[sigSize];
+
+        for (int i = 0; i < sigSize; i++) {
+            sig[i] = -1;
+        }
+    }
+
+    public OnePermHashSig(int[] sig) {
+        sigSize = sig.length;
+        this.sig = new int[sigSize];
+        this.sig = sig;
+    }
+
+    public boolean equals(OnePermHashSig otherSig) {
+        for (int i = 0; i < sigSize; i++) {
+            if (sig[i] != otherSig.sig[i]) { return false; }
+        }
+
+        return true;
+    }
+
+    public boolean unassigned() {
+        for (int i = 0; i < sigSize; i++) {
+            if (sig[i] != -1) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    public static int compare(OnePermHashSig a, OnePermHashSig b) {
+        for (int i = 0; i < a.sigSize; i++) {
+            if (a.sig[i] != b.sig[i]) {
+                return a.sig[i] - b.sig[i];
+            }
+        }
+
+        return 0;
+    }
+}
